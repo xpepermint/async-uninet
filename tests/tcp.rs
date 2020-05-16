24e82dd46445b7a3f81330c828a898d9b4132b60
Kristijan Sedlak <xpepermint@gmail.com>
2020-05-16 16:58:58 +0200

    Add tests

--- tests/tcp.rs
+++ tests/tcp.rs
@@ -18,3 +18,15 @@ async fn starts_inet_server() {
     }
     assert!(result);
 }
+
+#[async_std::test]
+async fn performs_http_request() {
+    let address = SocketAddr::from_str("google.com:80").await.unwrap();
+    let mut stream = Stream::connect(&address).await.unwrap();
+    stream.write_all(b"GET / HTTP/1.0\r\n\r\n").await.unwrap();
+
+    let mut res = Vec::new();
+    let length = stream.read_to_end(&mut res).await.unwrap();
+    
+    assert!(length > 0);
+}
